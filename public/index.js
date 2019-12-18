def83e5ce527a451a4185ef76349ad15a6be5a1e
bbishay <50244538+bbishay@users.noreply.github.com>
2019-12-18 11:45:22 -0500

    more stuff

--- public/index.js
+++ public/index.js
@@ -1,16 +1,38 @@
 const path = require('path');
+const hbs = require('hbs');
 const express = require('express');
 const app = express();
 
 //define paths for express
 const publicDirectoryPath = path.join(__dirname, '../public');
+const partialsPath = path.join(__dirname, '../templates/partials')
 const viewsPath = path.join(__dirname, '../templates/views');
 //setup handlebars engine and views location
-app.set('views engine', 'hbs'); //telling express/node to use hbs for templates
+app.set('view engine', 'hbs'); //telling express/node to use hbs for templates
 app.set('views', viewsPath); //telling express to get templates from templates/views/folder
+hbs.registerPartials(partialsPath);
 app.get("", async (req, res) => {
     try {
-        res.render('index');
+        res.render('index', {});
+    } catch (error) {
+        res.status(500).send('Page not found');
+    }
+});
+
+app.get('/swag', async (req, res) => {
+    try{
+        res.render('swag', {});
+    } catch(error){
+        res.status(500).send('Page not found')
+    }
+});
+
+app.get("/about/:id", async (req, res) => {
+    const name = req.params.id;
+    try {
+        res.render('index', {
+            title: name
+        });
     } catch (error) {
         res.status(500).send('Page not found');
     }
